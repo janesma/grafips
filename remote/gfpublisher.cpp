8d3942adc3ea0746fcbf85180ee14f7570675c57
Mark Janes <mark.a.janes@intel.com>
2014-12-02 17:01:58 -0800

    added style conformance for some remote files.

--- remote/gfpublisher.cpp
+++ remote/gfpublisher.cpp
@@ -1,23 +1,48 @@
-#include "gfpublisher.h"
-#include "gfimetric_source.h"
-#include "gfisubscriber.h"
+// Copyright (C) Intel Corp.  2014.  All Rights Reserved.
 
-using namespace Grafips;
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
 
-PublisherImpl::PublisherImpl() : m_subscriber(NULL)
-{}
+// The above copyright notice and this permission notice (including the
+// next paragraph) shall be included in all copies or substantial
+// portions of the Software.
 
-PublisherImpl::~PublisherImpl() 
-{
-}
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE COPYRIGHT OWNER(S) AND/OR ITS SUPPLIERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-void 
-PublisherImpl::RegisterSource(MetricSourceInterface *p)
-{
+//  **********************************************************************/
+//  * Authors:
+//  *   Mark Janes <mark.a.janes@intel.com>
+//  **********************************************************************/
+
+#include "remote/gfpublisher.h"
+
+#include <vector>
+
+#include "qml/sources/gfimetric_source.h"
+#include "remote/gfisubscriber.h"
+
+using Grafips::PublisherImpl;
+
+PublisherImpl::PublisherImpl() : m_subscriber(NULL) {}
+
+PublisherImpl::~PublisherImpl() {}
+
+void
+PublisherImpl::RegisterSource(MetricSourceInterface *p) {
     std::vector<MetricDescription> desc;
     p->GetDescriptions(&desc);
-    for (unsigned int i = 0; i < desc.size(); ++i)
-    {
+    for (unsigned int i = 0; i < desc.size(); ++i) {
         m_sources_by_metric_id[desc[i].id()] = p;
     }
 
@@ -28,32 +53,29 @@ PublisherImpl::RegisterSource(MetricSourceInterface *p)
         Subscribe(m_subscriber);
 }
 
-void 
-PublisherImpl::OnMetric(const DataSet &d)
-{
+void
+PublisherImpl::OnMetric(const DataSet &d) {
     if (m_subscriber)
         m_subscriber->OnMetric(d);
 }
 
-void 
-PublisherImpl::Enable(int id)
-{
+void
+PublisherImpl::Enable(int id) {
     m_sources_by_metric_id[id]->Enable(id);
 }
 
-void 
-PublisherImpl::Disable(int id)
-{
+void
+PublisherImpl::Disable(int id) {
     m_sources_by_metric_id[id]->Disable(id);
     m_subscriber->Clear(id);
 }
 
-void 
-PublisherImpl::Subscribe(SubscriberInterface *s)
-{
+void
+PublisherImpl::Subscribe(SubscriberInterface *s) {
     m_subscriber = s;
     std::vector<MetricDescription> descriptions;
-    for (std::vector<MetricSourceInterface *>::const_iterator i = m_sources.begin(); i != m_sources.end(); ++i)
+    for (std::vector<MetricSourceInterface *>::iterator i = m_sources.begin();
+         i != m_sources.end(); ++i)
         (*i)->GetDescriptions(&descriptions);
     s->OnDescriptions(descriptions);
 }
